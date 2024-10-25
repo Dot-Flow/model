9249bb64036c55d43e3d08f05db246dfdd786ef0
khmute <khmute@gmail.com>
2024-10-25 17:42:23 +0900

    정답 데이터 이미지 매핑 알고리즘 추가

--- L5_FeedbackGenerator/feedback_gen.py
+++ L5_FeedbackGenerator/feedback_gen.py
@@ -60,48 +60,73 @@ def generate_ground_truth(predicted_brl, corrected_brl, image_coordinates):
                 'correct_char': corrected_brl[c_idx],
                 'coordinates': None
             })
-    for gt in ground_truth:
-        print(gt)
+    
     return ground_truth
 
+def brl_to_labels(brl_by_lines):
+    labels_by_lines = []
+    for brl in brl_by_lines:
+        labels = []
+        for char in brl:
+            if char == '⠀':
+                continue
+            label = ord(char) - 0x2800
+            labels.append(label)
+        labels_by_lines.append(labels)
+    return labels_by_lines
 
 def main(extracted_json):
     predicted_brl_by_lines = extracted_json['prediction']['brl']
     corrected_brl_by_lines = extracted_json['correction']['brl']
-    labels_by_lines = extracted_json['prediction']['labels']
     boxes_by_lines = extracted_json['prediction']['boxes']
     
     img = Image.open('test.jpg')
     draw = ImageDraw.Draw(img)
     
-    ground_truth_by_lines = []
+    corrected_coordinates_by_lines = []
     for predicted_brl, corrected_brl, boxes in zip(predicted_brl_by_lines, corrected_brl_by_lines, boxes_by_lines):
+        # print(corrected_brl)
         ground_truth = generate_ground_truth(predicted_brl, corrected_brl, boxes)
+        corrected_coordinates = []
         for gt in ground_truth:
-            if gt['ocr_char'] != gt['correct_char']:
-                print(gt)
-                coordinates = gt['coordinates']
-                if coordinates is None:
-                    """
-                    TODO: 이미지 좌표가 없는 경우 처리
-                    case 1. OCR이 인식하지 못한 문자
-                        -> 줄의 시작이나 끝에 있는 문자일 가능성이 높음.
-                        -> 이에 대한 처리가 필요함.
-                    case 2. 교정 결과에 없는 문자
-                        -> 점자가 아닌 부분을 점자로 인식한 경우일 가능성이 높음.
-                        -> 이에 대한 처리가 필요함.
-                    """
-                    continue
-                if not isinstance(coordinates, (list, tuple)):
-                    coordinates = list(coordinates)
-                draw.rectangle(coordinates, outline='red')
-                char_to_draw = str(ord(gt['ocr_char']) - 0x2800) + "->" + str(ord(gt['correct_char']) - 0x2800)
-                draw.text((coordinates[0], coordinates[1] - 10), char_to_draw, fill='black')
+            coordinates = gt['coordinates']
             
+            if coordinates is None:
+                """
+                TODO: 이미지 좌표가 없는 경우 처리
+                case 1. OCR이 인식하지 못한 문자
+                    -> 줄의 시작이나 끝에 있는 문자일 가능성이 높음.
+                case 2. 교정 결과에 없는 문자
+                    -> 점자가 아닌 부분을 점자로 인식한 경우일 가능성이 높음.
+                """
+                pass
+            
+            if gt['ocr_char'] == gt['correct_char']:
+                if gt['correct_char'] != '⠀':
+                    print(gt)
+                    corrected_coordinates.append(coordinates)
+                continue
+    
+            
+            corrected_coordinates.append(coordinates)
+            
+            draw.rectangle(coordinates, outline='red')
+            char_to_draw = str(ord(gt['ocr_char']) - 0x2800) + "->" + str(ord(gt['correct_char']) - 0x2800)
+            draw.text((coordinates[0], coordinates[1] - 10), char_to_draw, fill='black')
+        corrected_coordinates_by_lines.append(corrected_coordinates)
+    
+    extracted_json['correction']['boxes'] =  corrected_coordinates_by_lines
+    extracted_json['correction']['labels'] = brl_to_labels(corrected_brl_by_lines)
     img.save('result.jpg')
+    print('Done.')
+    print('Result image is saved as result.jpg')
+    print('Retruned JSON is updated.')
+    return extracted_json
     
     
 if __name__ == '__main__':
     with open('test.json', 'r') as f:
         extracted_json = json.load(f)
-    main(extracted_json)
+
+    with open('test.json', 'w') as f:
+        json.dump(main(extracted_json), f, indent=4, ensure_ascii=False)
