33e324d8db71ca07c25618a58c2e2b1bd3d09199
khmute <khmute@gmail.com>
2024-11-04 17:01:51 +0900

    S3 연결

--- L1_OCR/run_ocr_app.py
+++ L1_OCR/run_ocr_app.py
@@ -4,23 +4,38 @@
 Local application for Angelina Braille Reader inference
 """
 import os
+import boto3
+
 from pathlib import Path
 import PIL.Image
 
 import L1_OCR.local_config as local_config
 import L1_OCR.model.infer_retinanet as infer_retinanet
 
-def run_ocr(image):
-    model_weights = 'model.t7'
-    
-    recognizer = infer_retinanet.BrailleInference(
-        params_fn=os.path.join(local_config.data_path, 'weights', 'param.txt'),
-        model_weights_fn=os.path.join(local_config.data_path, 'weights', model_weights),
-        create_script=None)
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+bucket_name = os.environ['BUCKET_NAME']
+file_key = os.environ['FILE_KEY']
 
-    # uuid_int = recognizer.get_uuid()
-    # print('UUID: ' + str(uuid_int))
-    
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name='ap-northeast-2'
+)
+
+# model_weights = 'model.t7'
+local_model_path = os.path.join(local_config.data_path, 'weights', 'model_weights.pth')
+
+with open(local_model_path, 'wb') as f:
+    s3.download_fileobj(bucket_name, file_key, f)
+
+recognizer = infer_retinanet.BrailleInference(
+    params_fn=os.path.join(local_config.data_path, 'weights', 'param.txt'),
+    model_weights_fn=local_model_path,
+    create_script=None
+)
+def run_ocr(image):
     results_dir = local_config.data_path
 
     recognizer.run_and_save(image, results_dir, target_stem=None,
